99b58fb9dcd2ad56c3a4107a723cfc99748f96cb
Alex Baker <baker.alex.j@gmail.com>
2020-02-07 15:00:12 -0700

    Added Conrad's screens to the list

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ListScreen from './src/screens/ListScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
 const navigator = createStackNavigator(
   {
+    Home: HomeScreen,
+    List: ListScreen,
     Login: LoginScreen,
     Signup: SignupScreen
   },
