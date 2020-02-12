e014ec3550f3414e4640f1899a4ac6ab2e83e5a3
Alex Baker <baker.alex.j@gmail.com>
2020-02-11 17:57:29 -0700

    Add test screen.

--- App.js
+++ App.js
@@ -4,18 +4,22 @@ import HomeScreen from './src/screens/HomeScreen';
 import ListScreen from './src/screens/ListScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import TestScreen from './src/screens/TestScreen';
+import Firebase from './constants/FirebaseKeys';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     List: ListScreen,
     Login: LoginScreen,
-    Signup: SignupScreen
+    Signup: SignupScreen,
+    Test: TestScreen
   },
   {
     initialRouteName: 'Login',
     defaultNavigationOptions: {
-      title: 'Yolked'
+      title: 'Yolked',
+      headerShown: false
     }
   }
 );
