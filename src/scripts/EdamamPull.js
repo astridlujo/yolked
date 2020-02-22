33289051ab4507c9e1891b90274e01d30a85c9b5
Alex Baker <baker.alex.j@gmail.com>
2020-02-21 22:48:53 -0700

    Pagination for search added.

--- src/scripts/EdamamPull.js
+++ src/scripts/EdamamPull.js
@@ -3,7 +3,7 @@ import EdamamKeys from '../../constants/EdamamKeys';
 const foodRoot = 'https://api.edamam.com/api/food-database/';
 const recipeRoot = 'https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}';
 
-async function getJson(url) {
+function getJson(url) {
   return fetch(url, {
     method: 'GET',
   }).then(response => {
@@ -18,7 +18,7 @@ async function getJson(url) {
   })
 }
 
-async function postJson(url, data) {
+function postJson(url, data) {
   return fetch(url, {
     method: 'POST',
     body: data
@@ -31,22 +31,50 @@ export async function SearchFood(foodName) {
                 + `&app_id=${EdamamKeys.foodAppId}&app_key=${EdamamKeys.foodAppKey}`;
   searchUrl = searchUrl.concat(urlParams);
 
-  console.log(searchUrl);
-  //console.log("Function working");
   console.log(foodName);
 
   const foodResponse = getJson(searchUrl);
-
-  let response = 'default';
+  let response = new Array();
 
   await foodResponse.then(data => {
-    //console.log(data);
-    //console.log(data['hints']);
-    console.log("LENGTH: " + data['hints'].length)
-    response = data['hints'];
+    console.log("LENGTH: " + data.hints.length)
+    if (data.hints !== undefined) {
+      response.push({hints: data.hints});
+    } else {
+      response.push({hints: undefined});
+    }
+    //toAppend.push({hints: data.hints});
+    if (data._links !== undefined) {
+      response.push({nHref: data._links.next.href});
+    } else {
+      response.push({nHref: undefined});
+    }
+    response.push({pHref: searchUrl})
   })
 
   return response;
+}
+
+export async function GetMore(href) {
+  const response = getJson(href);
+  let toAppend = new Array();
+
+  await response.then(data => {
+    if (data.hints !== undefined) {
+      toAppend.push({hints: data.hints});
+    } else {
+      toAppend.push({hints: undefined});
+    }
+    //toAppend.push({hints: data.hints});
+    if (data._links !== undefined) {
+      toAppend.push({nHref: data._links.next.href});
+    } else {
+      toAppend.push({nHref: undefined});
+    }
+    //toAppend.push({href: data._links.next.href});
+  });
+
+  return toAppend;
 }
 
 export async function SearchRecipe(recipeName) {
@@ -61,48 +89,14 @@ export async function SearchRecipe(recipeName) {
 
   const foodResponse = getJson(searchUrl);
 
-  let response = 'default';
+  let response = [];
 
   await foodResponse.then(data => {
     //console.log(data);
     //console.log(data['hints']);
     console.log("LENGTH: " + data['hits'].length)
-    response = data['hints'];
-  })
+    response = data['hits'];
+  });
 
   return response;
 }
-
-
-//
-// export default class EdamamPull extends React.Component {
-//   constructor(searchText = '') {
-//     this.searchText = '';
-//     this.result = '';
-//
-//     this.rootURL = 'https://api.edamam.com/api/food-database/';
-//     //this.urlParams = URLSearchParams([['app_id',EdamamKeys.foodAppId],['app_key',EdamamKeys.foodAppKey]])
-//     //this.urlParams = URLSearchParams(`app_id=${EdamamKeys.foodAppId}&app_key=${EdamamKeys.foodAppKey}`)
-//   }
-//   searchFood(searchText) {
-//     this.searchText = searchText;
-//
-//     this.urlParams = URLSearchParams();
-//     this.uriParams.append('ingr', searchText);
-//     urlSearch = this.urlParams.toString();
-//     console.log(urlSearch);
-//   }
-//
-//   async getJson(url) {
-//     const response = await fetch(url, {
-//       method: 'GET',
-//     })
-//   }
-//
-//   async postJson(url, data) {
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       body: data
-//     })
-//   }
-// }
