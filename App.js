b978ab4259861de4d6de0337c82be9c73d48ff80
Alex Baker <baker.alex.j@gmail.com>
2020-02-21 22:52:41 -0700

    IMPORTANT: Remove YellowBox warnings for timer. added test screen, foodscreen.

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import ListScreen from './src/screens/ListScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
-import TestScreen from './src/screens/TestScreen';
+import TestScreen from './src/screens/TestScreen'; // Mark for removal....
 import FoodScreen from './src/screens/FoodScreen';
+import CategoriesScreen from './src/screens/FoodScreen';
 
 const navigator = createStackNavigator(
   {
