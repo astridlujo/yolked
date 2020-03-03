0a8af54a08c536faee3cc18e28dafcc359fd8245
Alex Baker <baker.alex.j@gmail.com>
2020-03-03 12:20:33 -0700

    Nutrients map

---
+++ constants/Nutrients.js
@@ -0,0 +1,30 @@
+export const NutrientMap = [
+  { nutrient: 'CA', content: { name: 'Calcium', unit: 'mg' }},
+  { nutrient: 'CHOCDF', content: { name: 'Carbs', unit: 'g' }},
+  { nutrient: 'CHOLE', content: { name: 'Cholesterol', unit: 'mg' }},
+  { nutrient: 'FAMS', content: { name: 'Monosaturated Fat', unit: 'g' }},
+  { nutrient: 'FAPU', content: { name: 'Polyunsaturated Fat', unit: 'g' }},
+  { nutrient: 'FASAT', content: { name: 'Unsaturated Fat', unit: 'g' }},
+  { nutrient: 'FAT', content: { name: 'Fat', unit: 'g' }},
+  { nutrient: 'FATRN', content: { name: 'Trans Fat', unit: 'g' }},
+  { nutrient: 'FE', content: { name: 'Iron', unit: 'mg' }},
+  { nutrient: 'FIBTG', content: { name: 'Fiber', unit: 'g' }},
+  { nutrient: 'FOLDFE', content: { name: 'Folate (Equivalent)', unit: 'mcg' }}, // Marked æg
+  { nutrient: 'K', content: { name: 'Potassium', unit: 'mg' }},
+  { nutrient: 'MG', content: { name: 'Magnesium', unit: 'mg' }},
+  { nutrient: 'NA', content: { name: 'Sodium', unit: 'mg' }},
+  { nutrient: 'ENERC_KCAL', content: { name: 'Energy', unit: 'kcal' }},
+  { nutrient: 'NIA', content: { name: 'Niacin', unit: 'mg' }},
+  { nutrient: 'P', content: { name: 'Phosphorus', unit: 'mg' }},
+  { nutrient: 'PROCNT', content: { name: 'Protein', unit: 'g' }},
+  { nutrient: 'RIBF' , content: { name: 'Riboflavin (B2)', unit: 'mg' }},
+  { nutrient: 'SUGAR', content: { name: 'Sugars', unit: 'g' }},
+  { nutrient: 'THIA', content: { name: 'Thiamin (B1)', unit: 'mg' }},
+  { nutrient: 'TOCPHA', content: { name: 'Vitamin E', unit: 'mg' }},
+  { nutrient: 'VITA_RAE', content: { name: 'Vitamin A', unit: 'IU' }}, // Marked æg
+  { nutrient: 'VITB12', content: { name: 'Vitamin B12', unit: 'mcg' }}, // Marked æg
+  { nutrient: 'VITB6A', content: { name: 'Vitamin B6', unit: 'mg' }},
+  { nutrient: 'VITC', content: { name: 'Vitamin C', unit: 'mg' }},
+  { nutrient: 'VITD', content: { name: 'Vitamin D', unit: 'IU' }}, // Marked æg
+  { nutrient: 'VITK1', content: { name: 'Vitamin K', unit: 'mcg' }}, // Marked æg
+];
