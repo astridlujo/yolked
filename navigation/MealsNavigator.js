71decc9b691eab2c00bfd38a76c31558871586fa
astridvargas <32526308+astridvargas@users.noreply.github.com>
2020-03-03 13:47:33 -0700

    Made the bottom tab navigation with icons
    Updated the navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,13 +1,23 @@
 // A screen navigator
+import React from 'react';
+import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 // Import screens
 import CategoriesScreen from '../src/screens/CategoriesScreen';
 import CategoryMealsScreen from '../src/screens/CategoryMealsScreen';
 import MealDetailScreen from '../src/screens/MealDetailScreen'
+import HomeScreen from '../src/screens/HomeScreen';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons, Foundation } from '@expo/vector-icons';
 
 import Colors from '../constants/Colors';
+import SearchRecipeScreen from '../src/screens/SearchRecipeScreen';
+import FavoriteScreen from '../src/screens/FavoriteScreen'
+import ListScreen from '../src/screens/ListScreen'
+
 
 const MealsNavigator = createStackNavigator(
     {
@@ -20,6 +30,9 @@ const MealsNavigator = createStackNavigator(
         MealDetail: {
             screen: MealDetailScreen
         },
+        HomeScreen: {
+            screen: HomeScreen
+        }
     },
 
     {
@@ -33,4 +46,87 @@ const MealsNavigator = createStackNavigator(
     }
 );
 
-export default createAppContainer(MealsNavigator);
+const bottomTabNavigatorConfig = {
+    // The Recipes screen
+    RecipesScreen: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarLabel: 'Recipes',
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons 
+                        name='ios-restaurant' 
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            }
+        }
+    },
+        
+    // The Search Recipes Screen
+    RecipesSearchScreen: {
+        screen: SearchRecipeScreen,
+        navigationOptions: {
+            tabBarLabel: 'Search Recipes',
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Foundation
+                        name='page-search'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            }
+        }
+    },
+    
+    // The pantry Screen
+    PantryScreen: {
+        screen: ListScreen,
+        navigationOptions: {
+            tabBarLabel: 'Pantry',
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons 
+                        name='ios-search'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            }
+        }
+    },
+    
+    // The Favorite Items Screen
+    FavoritesScreen: {
+        screen: FavoriteScreen,
+        navigationOptions: {
+            tabBarLabel: 'Favorites',
+            tabBarIcon: (tabInfo) => {
+                return (
+                    <Ionicons 
+                        name='ios-star'
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                );
+            }
+        }
+    }
+}
+
+
+const appTabNavigator = 
+    Platform.OS === 'android' 
+        ? createMaterialBottomTabNavigator(bottomTabNavigatorConfig, {
+            activeColor: Colors.accentColor,
+            shifting: true
+        }) 
+        : createBottomTabNavigator(bottomTabNavigatorConfig, {
+            tabBarOptions: {
+                activeTintColor: Colors.headerButtonColor
+    }
+});
+
+export default createAppContainer(appTabNavigator);
